4498dfb9da65742ba8ef6217e3df0b9a309aa980
motionss <danialfonso215@gmail.com>
2023-01-23 03:54:58 -0300

    test

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-from typing import Union
+import os
+import http.server
+import socketserver
 
-from fastapi import FastAPI
-
-app = FastAPI()
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+from http import HTTPStatus
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(HTTPStatus.OK)
+        self.end_headers()
+        msg = 'Python is running on Qoddi! You requested %s' % (self.path)
+        self.wfile.write(msg.encode())
+
+
+port = int(os.getenv('PORT', 8080))
+print('Listening on port %s' % (port))
+httpd = socketserver.TCPServer(('', port), Handler)
+httpd.serve_forever()
